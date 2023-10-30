8d1fbde59cce350c47d4a478693b18e97967d0d2
Jannis Rosenbaum <j.ro@dock51.de>
2023-10-30 14:07:32 +0200

    increment timeouts

--- crates/base/src/deno_runtime.rs
+++ crates/base/src/deno_runtime.rs
@@ -1593,8 +1593,8 @@ mod test {
             Some(WorkerRuntimeOpts::UserWorker(UserWorkerRuntimeOpts {
                 memory_limit_mb,
                 worker_timeout_ms,
-                cpu_time_soft_limit_ms: 100,
-                cpu_time_hard_limit_ms: 200,
+                cpu_time_soft_limit_ms: 500,
+                cpu_time_hard_limit_ms: 500,
                 force_create: true,
                 ..default_opt
             })),
